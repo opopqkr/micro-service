70f265079219710b76e7b6cae7158d59ad39ffb4
cmpark <opopqkr1@gmail.com>
2025-02-21 13:10:06 +0900

    user-service commit

--- user-service/src/main/java/com/study/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/study/user_service/UserServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("TEST");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 

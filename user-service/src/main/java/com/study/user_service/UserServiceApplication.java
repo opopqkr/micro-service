9ac228afd97d3b121fe00ff4bfa76afd640a8e1e
cmpark <opopqkr1@gmail.com>
2025-02-21 13:10:34 +0900

    user-service commit

--- user-service/src/main/java/com/study/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/study/user_service/UserServiceApplication.java
@@ -1,15 +1,17 @@
 package com.study.user_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("TEST");
+		log.debug("### user-service start ###");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 

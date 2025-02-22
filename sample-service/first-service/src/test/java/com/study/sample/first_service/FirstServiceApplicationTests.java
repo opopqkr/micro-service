a75976528a37518c9d29f2ec0e3cfac8f63a9e9a
cmpark <opopqkr1@gmail.com>
2025-02-22 10:38:32 +0900

    Netflix Zuul study

---
+++ sample-service/first-service/src/test/java/com/study/sample/first_service/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sample.first_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sample-service/zuul-service/src/main/java/com/study/sample/zuul_service/filter/ZuulLoggingFilter.java
@@ -0,0 +1,42 @@
+package com.study.sample.zuul_service.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    // private final Logger log = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("************* Request Infos : ");
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info("************* {}", request.getRequestURI());
+
+        return null;
+    }
+
+}

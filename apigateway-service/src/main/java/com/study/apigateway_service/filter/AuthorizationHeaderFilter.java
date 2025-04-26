d2a6c0ac00f8ada69aeb53aab6d9fdfbd5c805e4
cmpark <opopqkr1@gmail.com>
2025-04-26 12:14:29 +0900

    Authorization filter add

---
+++ apigateway-service/src/main/java/com/study/apigateway_service/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,86 @@
+package com.study.apigateway_service.filter;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment environment;
+
+    public AuthorizationHeaderFilter(Environment environment) {
+        super(Config.class);
+        this.environment = environment;
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header.");
+            }
+
+            String authorizationHeader = Objects.requireNonNull(request.getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+            String jwt = authorizationHeader.replace("Bearer", "");
+
+            if (!isJWtValid(jwt)) {
+                return onError(exchange, "JWT is not valid.");
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    // Mono, Flux -> Spring WebFlux
+    private Mono<Void> onError(ServerWebExchange exchange, String errorMessage) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+        log.error(errorMessage);
+        return response.setComplete();
+    }
+
+    private boolean isJWtValid(String jwt) {
+        boolean isValid = true;
+
+        String subject = null;
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(environment.getProperty("token.secret"))
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+            isValid = false;
+        }
+
+        if (StringUtils.isBlank(subject))
+            isValid = false;
+
+        return isValid;
+    }
+
+}

7fd1ec1ff3f3e3772d8193df84a85c77cec020bf
cmpark <opopqkr1@gmail.com>
2025-05-20 22:18:09 +0900

    @LoadBalanced 주석에 관한 어노테이션 추가

--- user-service/src/main/java/com/study/user_service/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/study/user_service/config/RestTemplateConfig.java
@@ -9,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    @LoadBalanced // 다른 마이크로 서비스 호출 시, discovery server 에 등록되어 있는 micro-service 명으로 호출 가능
+    @LoadBalanced /* 다른 마이크로 서비스 호출 시, discovery server 에 등록되어 있는 micro-service 명으로 호출 가능
+                     단, api-gateway 거치지 않고 즉시 micro-service 호출. */
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
